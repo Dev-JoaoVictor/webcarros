c61ee31350e250d12e354d1bbea8fff5e9a5d42b
João <joaovictor.ramos34@gmail.com>
2023-12-13 14:37:30 -0300

    feat(layout): criando o layout padrao das paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-export function App() {
-  return (
-    <>
-    </>
-  )
-}
+import { createBrowserRouter } from "react-router-dom";
 
+import { Home } from "./pages/home";
+import { Layout } from "./components/layout";
+
+export const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home /> }
+    ]
+  },
+])
